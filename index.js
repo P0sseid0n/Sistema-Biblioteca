d4a00ff18870d4a4991ac74dffefc5629b7e12ef
Posseidon <matheuspossidoniom@outlook.com>
2021-01-18 20:17:41 -0300

    PUT & DELETE

--- index.js
+++ index.js
@@ -36,7 +36,47 @@ app.post('/obras', (req, res) => {
 
     DB.push({ titulo, editora, foto, autores })
     res.status(200)
-    res.json({ status: "OK", txt: "Dados cadastrados" })
+    res.json({ status: "OK", txt: "Livro cadastrado" })
+})
+
+app.put('/obras/:id', (req, res) => {
+    const id = req.params.id
+
+    if(isNaN(id) || !DB[id]){
+        res.status(404)
+        res.json({ status: "Erro",  txt: "ID invalido" })
+        return
+    }
+
+    const { titulo, editora, foto, autores } = req.body
+
+    if(titulo == "" || editora == "" || foto == "" || autores.lenght == 0){
+        res.status(400)
+        res.json({ status: "Erro",  txt: "Campo vazio" })
+        return
+    }
+
+    if(titulo) DB[id].titulo = titulo
+    if(editora) DB[id].editora = editora
+    if(foto) DB[id].foto = foto
+    if(autores) DB[id].autores = autores
+
+    res.status(200)
+    res.json({ status: "OK", txt: "Livro atualizado" })
+})
+
+app.delete('/obras/:id', (req, res) => {
+    const id = req.params.id
+
+    if(isNaN(id) || !DB[id]){
+        res.status(404)
+        res.json({ status: "Erro",  txt: "ID invalido" })
+        return
+    }
+
+    delete DB[id]
+    res.status(200)
+    res.json({ status: "OK", txt: "Livro apagado" })
 })
 
 app.listen(PORT, () => console.log('Online'))
